609a9f921066e6232ac0859355e5bd4ca2340843
Reinis Veips <reinis.veips@wot.lv>
2012-02-27 09:22:38 +0200

    Added a feature to use callables in settings.py

--- social_auth/utils.py
+++ social_auth/utils.py
@@ -85,8 +85,19 @@ def group_backend_by_type(items, key=lambda x: x):
 
 
 def setting(name, default=None):
-    """Return setting value for given name or default value."""
-    return getattr(settings, name, default)
+    """Return setting value for given name or default value.
+    As of now, setting can be a callable- e.g. in settings.py:
+    def callable_setting(key):
+        def inner_fn():
+            from site_settings.helpers import get_setting
+            return get_setting(key)
+        return inner_fn
+    SOCIAL_AUTH_SETTING = callable_setting('SOCIAL_AUTH_SETTING')
+    """
+    ret = getattr(settings, name, default)
+    if hasattr(ret, '__call__'):
+        ret = ret()
+    return ret
 
 
 def backend_setting(backend, name, default=None):
